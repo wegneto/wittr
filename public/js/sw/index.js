c35403f1da010664a9047e3df4187b8c05c9b432
Wilson Guimarães <wegneto@gmail.com>
2017-11-13 21:33:35 +0000

    teste

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,6 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
+	// teste 
   console.log(event.request);
 });
